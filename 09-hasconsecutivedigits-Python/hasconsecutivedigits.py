7d02d8837a533cbbce69ae530c911437bd711f5f
Rohini58 <chavvarohini@msitprogram.net>
2021-07-19 11:53:44 +0530

    commit

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -3,5 +3,14 @@
 # number contains two consecutive digits that are the same, and False otherwise.
 
 def hasconsecutivedigits(n):
+	n = abs(n)
+	prevDigit = -1
+	while (n > 0):
+		onesDigit = n%10
+		n //= 10
+		if (prevDigit == onesDigit):
+			return True
+		prevDigit = onesDigit
+	return False
 	# your code goes here
 	pass
