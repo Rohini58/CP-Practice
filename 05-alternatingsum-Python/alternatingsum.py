87a825f56a3ef38a098a1d11c5daddf129aa5050
Rohini58 <chavvarohini@msitprogram.net>
2021-07-20 21:34:45 +0530

    commit

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -5,7 +5,49 @@
 
 
 
+
+    # l=[]
+    # l1=[]
+    # for i in a:
+    #     if i%2==0:
+    #         l.append(i)
+    #     else:
+    #         l1.append(i)
+    # sub=l-l1
+    # return sub
+            
+
+def readArray():
+    a = []
+    l = int(input())
+    for i in range(l):
+        a.append(int(input()))
+    return a
+
 def fun_alternatingsum(a): 
-	return 0
+    # your code goes here
+    flag=True
+    res=0
+    for i in a:
+        if(flag==True):
+            res=res+i
+            flag=False
+        else:
+            res=res-i
+            flag=True
+    return res
+
+            
+            
+    
 
 
+
+# def alternatingSum(L):
+#     altSum=0
+# for i in range(len(L)):
+# if i%2==0:
+# altSum+=L[i]
+# else:
+# altSum-=L[i]
+# return altSum
