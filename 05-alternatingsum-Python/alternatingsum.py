0b4079109db1db1e4f097cbc15f1bb1c5cbfc994
Rohini58 <chavvarohini@msitprogram.net>
2021-07-20 21:53:51 +0530

    commit

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -4,8 +4,14 @@
 
 
 
-
-            
+def fun_alternatingsum(a):
+    altSum=0
+    for i in range(len(a)):
+        if i%2==0:  #i  takes index not a value 
+            altSum+=a[i]
+        else:
+            altSum-=a[i]
+    return altSum  
 
 # def readArray():
 #     a = []
@@ -14,18 +20,18 @@
 #         a.append(int(input()))
 #     return a
 
-def fun_alternatingsum(a): 
-    # your code goes here
-    # flag=True
-    # res=0
-    # for i in a:
-    #     if(flag==True):
-    #         res=res+i
-    #         flag=False
-    #     else:
-    #         res=res-i
-    #         flag=True
-    # return res
+# def fun_alternatingsum(a): 
+#     # your code goes here
+#     # flag=True
+#     # res=0
+#     # for i in a:
+#     #     if(flag==True):
+#     #         res=res+i
+#     #         flag=False
+#     #     else:
+#     #         res=res-i
+#     #         flag=True
+#     # return res
 
             
             
@@ -34,10 +40,10 @@ def fun_alternatingsum(a):
 
 
 # def alternatingSum(L):
-    altSum=0
-    for i in range(len(a)):
-        if i%2==0:
-            altSum+=a[i]
-        else:
-            altSum-=a[i]
-    return altSum
+    # altSum=0
+    # for i in range(len(a)):
+    #     if i%2==0:  #i  takes index not a value 
+    #         altSum+=a[i]
+    #     else:
+    #         altSum-=a[i]
+    # return altSum
