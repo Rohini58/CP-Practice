be5c7ab2f848f99f5327a4930c18724a379510e1
Rohini58 <chavvarohini@msitprogram.net>
2021-07-25 20:26:21 +0530

    commit

--- VowelCount/vowelcount.py
+++
@@ -1,9 +0,0 @@
-
-def vowelCount(s):
-    count=0
-    
-    for i in range(len(s)):
-        if("a"==s[i] or "e"==s[i] or "i"==s[i] or "o"==s[i] or "u"==s[i]):
-            count=count+1
-    return count
-print(vowelCount("aercious"))
