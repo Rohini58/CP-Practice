724b7d8cf1c2357fbb72164380114f4d936df36e
Rohini58 <chavvarohini@msitprogram.net>
2021-08-03 11:11:02 +0530

    commit

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,56 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def sum_of_digits(x):
+    if x <= 10:
+        return x
+    
+    sum = 0
+    
+    while(x != 0):
+        sum += x % 10
+        x = x // 10
+    return sum
 
-def fun_nth_smithnumber(n):
-    return 1
+def factorsofnumber(x):
+    if x <= 1:
+        return False
+    
+    primefactors = list()
+    sumfactors = 0
+    d = 2
+    
+    while(d <= x):
+        if x % d == 0:
+            primefactors.append(d)
+            x = x // d
+        else:
+            d += 1
+
+    for i in primefactors:
+        sumfactors += sum_of_digits(i)
+
+    return (sumfactors )
+
+
+def solve (n):
+    
+    if factorsofnumber(n) == sum_of_digits(n):
+        return  1
+    else:
+        return 0
+
+  
+
+def fun_nth_smithnumber(x):
+    found=0
+    guess=0
+    while(found<=x):
+        guess+=1
+        if(solve(guess)):
+            found+=1
+    return guess
+
+   
+
+   
