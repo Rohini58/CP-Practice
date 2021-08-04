5e6c0d4c9b16bfa909623ce644ca57e64a0557f4
Rohini58 <chavvarohini@msitprogram.net>
2021-08-04 15:03:21 +0530

    commit

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -4,7 +4,17 @@
 # that is in the first string, is in the second, and vice versa -- and False otherwise. This test is 
 # case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
 # parameter is not a string, but returns True if both strings are empty (why?).
-
 def samechars(s1, s2):
-	# Your code goes here
-	pass
+    s1=str(s1)
+    s2=str(s2)
+    # if s1==s2:
+    
+    for i in range(len(s2)):
+        if s2[i] in s1:
+            continue
+        else:
+            return False
+    return True
+  
+    # return False
+    
