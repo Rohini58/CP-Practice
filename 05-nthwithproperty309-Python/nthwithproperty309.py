c938f703c7116f700a54e1d9a202912e2c118d90
Rohini58 <chavvarohini@msitprogram.net>
2021-08-04 21:35:41 +0530

    comit

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -3,7 +3,28 @@
 # digit (from 0 to 9) at least once. 309 is the smallest number with this property. 
 # Write the function nthWithProperty309 that takes a non-negative int n and returns 
 # the nth number with Property309.
+def property309(n):
+    n=pow(n,5)
+    m=[]
+    while(n):
+        rem=n%10
+        m.append(rem)
+        n=n//10
+    x=[0,1,2,3,4,5,6,7,8,9]
+    for i in x:
+        if(i not in m):
+            return False
+    return True
 
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+    # Your code goes here
+    found=0
+    guess=0
+    while(found<=n):
+        if(property309(guess)):
+            if(found==n):
+                return guess
+            found+=1
+        guess+=1
+    return guess
+    # Your code goes here
